735c5f3199cd61da507644f1c83add6de6ebe3d5
Elizaveta Chulkova <chulkova_liza@mail.ru>
2023-03-16 01:21:18 +0300

    Fix InMemory and JDBC tests

---
+++ src/test/java/ru/javawebinar/topjava/service/jpa/AbstractJpaUserServiceTest.java
@@ -0,0 +1,17 @@
+package ru.javawebinar.topjava.service.jpa;
+
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.repository.JpaUtil;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+
+public class AbstractJpaUserServiceTest extends AbstractUserServiceTest {
+
+    @Autowired
+    protected JpaUtil jpaUtil;
+
+    @Before
+    public void setup() {
+        jpaUtil.clear2ndLevelHibernateCache();
+    }
+}
