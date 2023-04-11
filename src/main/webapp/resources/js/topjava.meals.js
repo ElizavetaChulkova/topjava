042f62b9953123747cb2ed55442ce2656bfb4619
Elizaveta Chulkova <chulkova_liza@mail.ru>
2023-04-11 14:08:02 +0300

    Plug datetimepicker to filter and update form

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -63,13 +63,51 @@ $(function () {
     };
     makeEditable();
 
-    $('#startDate, #endDate').datetimepicker({
+    var startDate = $('#startDate');
+    var endDate = $('#endDate');
+    startDate.datetimepicker({
         timepicker: false,
-        format: 'Y-m-d'
+        format: 'Y-m-d',
+        formatDate: 'Y-m-d',
+        onShow: function (ct) {
+            this.setOptions({
+                maxDate: endDate.val() ? endDate.val() : false
+            })
+        }
+    });
+    endDate.datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d',
+        formatDate: 'Y-m-d',
+        onShow: function (ct) {
+            this.setOptions({
+                minDate: startDate.val() ? startDate.val() : false
+            })
+        }
     });
 
-    $('#startTime, #endTime').datetimepicker({
+    var startTime = $('#startTime');
+    var endTime = $('#endTime');
+    startTime.datetimepicker({
         datepicker: false,
-        format: 'H:m'
+        format: 'H:i',
+        onShow: function (ct) {
+            this.setOptions({
+                maxTime: endTime.val() ? endTime.val() : false
+            })
+        }
+    });
+    endTime.datetimepicker({
+        datepicker: false,
+        format: 'H:i',
+        onShow: function (ct) {
+            this.setOptions({
+                minTime: startTime.val() ? startTime.val() : false
+            })
+        }
+    });
+
+    $('#dateTime').datetimepicker({
+        format: 'Y-m-d H:i'
     });
 });
